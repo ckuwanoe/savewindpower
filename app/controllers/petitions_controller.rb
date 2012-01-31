dfab3018a89548517cfab267def9bfcb23b63a18
Corey Kuwanoe <eschalon@gmail.com>
2012-01-31 15:15:28 -0800

    adding thanks

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -2,4 +2,21 @@ class PetitionsController < ApplicationController
   def index
     @petition = Petition.new
   end
+  
+  def create
+    @petition = Petition.new(params[:petition])
+
+    respond_to do |format|
+      if @petition.save
+        format.html { redirect_to(thanks_path, :notice => '') }
+        format.xml  { render :xml => @petition, :status => :created, :location => @petition }
+      else
+        format.html { render :action => "index" }
+        format.xml  { render :xml => @petition.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def thanks
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Savewindpower::Application.routes.draw do
+  match '/thanks', :to => 'petitions#thanks'
   resources :petitions
   root :to => 'petitions#index'
 end
