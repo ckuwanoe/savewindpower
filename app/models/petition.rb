0d7c0c62ad6e45598f5c881fb2e729cad69663fa
Corey Kuwanoe <eschalon@gmail.com>
2012-01-31 19:38:06 -0800

    adding validations

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -1,2 +1,9 @@
 class Petition < ActiveRecord::Base
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :presence => true
+  validates :address, :presence => true
+  validates :city, :presence => true
+  validates :state, :presence => true
+  validates :zip_code, :presence => true
 end
