7d1f0b07f6ac4bf4a173be096f4eafe38941f1dc
ckuwanoe <eschalon@gmail.com>
2013-01-25 16:40:14 -0800

    Update config/initializers/setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "eschalon",
-  :password             => "cr8tion",
+  :user_name            => "",
+  :password             => "",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
